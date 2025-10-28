6231776352abc53f9cd8cb224a826f25452bb362
Yakanksha K <147792127+YAKANKSHAKESAVAPATNAM@users.noreply.github.com>
2025-10-28 15:17:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Taking input/taking-input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+  public:
+    void inputData() {
+        int a;
+        string b;
+        cin >> a >> b;
+        cout << a << " " << b << endl;
+    }
+};
