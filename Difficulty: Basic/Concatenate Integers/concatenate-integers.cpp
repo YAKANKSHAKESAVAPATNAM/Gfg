59319a383eac7faa9c1c2e1e6fcc2bb266b02906
Yakanksha K <147792127+YAKANKSHAKESAVAPATNAM@users.noreply.github.com>
2025-11-09 22:30:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Concatenate Integers/concatenate-integers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+  string sa = to_string(a);
+  string sb = to_string(b);
+  cout << sa + sb << endl;
+  return 0;
+}
